1dcb11c329c125ab0ede319a7d56b092852e99c5
AMEER ROCK <naguriameer76@gmail.com>
2016-09-21 14:41:05 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello
+{
+public static void main(String args[])
+{
+String str="hello";
+String s= str.substring(4);
+System.out.println(s);
+System.out.println(s+str);
+System.out.println(s+str+s);
+}
+}
+
+
