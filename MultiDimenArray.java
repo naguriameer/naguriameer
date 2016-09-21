40745851c09b8e61720ba984afa639b69393331b
AMEER ROCK <naguriameer76@gmail.com>
2016-09-21 15:12:47 +0530

    Add files via upload

---
+++ MultiDimenArray.java
@@ -0,0 +1,9 @@
+public class MultiDimenArray{
+public static void main(String args[])
+{
+String [][] names={{"mr.","ms."},{"ram","gita","sita"}};
+System.out.println(names[0][0]+names[1][0]);
+System.out.println(names[0][1]+names[1][1]);
+System.out.println(names[0][1]+names[1][2]);
+}
+}
