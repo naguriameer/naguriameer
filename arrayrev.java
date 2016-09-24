056a5b0b1d16b7c87721a27d0a0ad6bf6fea98a9
AMEER ROCK <naguriameer76@gmail.com>
2016-09-24 16:26:09 +0530

    delete array

--- arrayrev.java
+++ arrayrev.java
@@ -1,39 +1,12 @@
-		
-import java.util.Scanner;
-
+import java.util.*;
 public class arrayrev
 {
-   public static void main(String args[])
-   {
-       int size, i, j, temp;
-       int arr[] = new int[50];
-       Scanner scan = new Scanner(System.in);
-	   
-       System.out.print("Enter Array Size : ");
-       size = scan.nextInt();
-	   
-       System.out.print("Enter Array Elements : ");
-       for(i=0; i<size; i++)
-       {
-           arr[i] = scan.nextInt();
-       }
-	   
-       j = i - 1;     
-       i = 0;         
-	   
-       while(i<j)
-       {
-           temp = arr[i];
-           arr[i] = arr[j];
-           arr[j] = temp;
-           i++;
-           j--;
-       }
-	   
-       System.out.print("Now the Reverse of Array is : \n");
-       for(i=0; i<size; i++)
-       {
-           System.out.print(arr[i]+ "  ");
-       }       
-   }
+public static void main(String[] args)
+{
+char a[]={'a','b','c','d'};
+for(char i=2;i<=a.length;i--)
+{
+System.out.println(a[i]);
+}
+}
 }
