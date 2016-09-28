3b8294e3530e1ca821abcb5b3cb9223093d298bb
AMEER ROCK <naguriameer76@gmail.com>
2016-09-28 15:17:32 +0530

    swap strings

---
+++ swap.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class swap
+{
+public static void main(String agrs[])
+{
+	String str1="ameer";
+	String temp;
+	String str2="asif";
+	System.out.println("before swaping names are:\n"+str1+"\n"+str2);
+	temp=str1;
+	str1=str2;
+	str2=temp;
+	System.out.println("after swaping names are:\n"+str1+"\n"+str2);
+	
+	}
+}
