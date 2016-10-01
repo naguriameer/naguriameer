9c054a33c369d20f7530926321a9aec604bc78c9
AMEER ROCK <naguriameer76@gmail.com>
2016-10-01 16:21:59 +0530

    Add files via upload

--- swapno.java
+++ swapno.java
@@ -1,20 +1,23 @@
 import java.io.*;
+import java.util.*;
 import java.util.Scanner;
 public class swapno
 {
 public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
-int a;
-int b;
+String str1;
+String str2;
 System.out.println("enter a b values");
-a=s.nextInt();
-b=s.nextInt();
-System.out.println("before swaping\n"+a+"\t"+b);
-a=a+b;
+str1=s.next();
+str2=s.next();
+System.out.println("before swaping\n"+str1+"\t"+str2);
 
-a=a-b;b=a-b;
-System.out.println("AFTER swaping\n"+a+"\t"+b);
+
+str1 = str1 + str2;
+str2 = str1.substring(0, (str1.length() - str2.length()));
+str1= str1.substring(str2.length());
+System.out.println("AFTER swaping\n"+str1+"\t"+str2);
 
 
 }
